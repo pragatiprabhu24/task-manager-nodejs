a9878c23372dee3cf1779eeffc00367e05bbf47b
pragatiprabhu24 <prabhupragati5@gmail.com>
2024-10-20 11:20:12 +0530

    fixed module not found error

--- routes/categories.js
+++ routes/categories.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Category = require("../models/Category");
+const Category = require("../models/category");
 const { isAuthenticated } = require("../middleware/auth");
 
 // create a new category
