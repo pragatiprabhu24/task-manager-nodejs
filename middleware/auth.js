3c5004b94495d9cf15696434d722028acd661e8f
pragatiprabhu24 <prabhupragati5@gmail.com>
2024-10-20 12:37:21 +0530

    added middleware for protected route

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,23 @@
-// Middleware to check if the user is authenticated
-function isAuthenticated(req, res, next) {
-  if (req.session && req.session.user) {
-    return next();
-  } else {
-    return res
-      .status(401)
-      .json({ message: "Unauthorized. Please log in first." });
+// middleware/auth.js
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const isAuthenticated = (req, res, next) => {
+  const token = req.headers["authorization"]?.split(" ")[1]; 
+
+  if (!token) {
+    return res.status(401).json({ message: "Access denied, no token provided" });
   }
-}
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded; 
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
 
 module.exports = { isAuthenticated };
